6671625d2db4ed31fafb4424b6d817c291ad783d
GitHub-Vadrigar <54599264+GitHub-Vadrigar@users.noreply.github.com>
2026-05-19 23:49:54 +0200

    test7

--- vadrigar-nerva/download.sh
+++ vadrigar-nerva/download.sh
@@ -1,52 +1,46 @@
 #!/bin/sh
 cd /data/nerva || exit 1
 
-echo "Ophalen hashes.txt..."
+echo "Receiving hashes.txt..."
 wget -qO hashes.txt https://github.com/nerva-project/nerva/releases/download/v0.2.1.0/hashes.txt
 
 check_and_download() {
   FILE="$1"
   URL="$2"
-  
-  # Zoek de verwachte hash op in het bestand
+
   EXPECTED_HASH=$(grep "$FILE" hashes.txt | awk '{print $1}')
   
   if [ -z "$EXPECTED_HASH" ]; then
-    echo "FOUT: Geen hash gevonden voor $FILE in hashes.txt"
+    echo "ERROR: No hash found for $FILE in hashes.txt"
     return 1
   fi
 
   if [ -f "$FILE" ]; then
     LOCAL_HASH=$(sha256sum "$FILE" | awk '{print $1}')
     if [ "$LOCAL_HASH" != "$EXPECTED_HASH" ]; then
-      echo "Update gevonden of corruptie in $FILE. Verwijderen..."
+      echo "Update found or corruption in $FILE. Deleting..."
       rm "$FILE"
     else
-      echo "$FILE is up-to-date en geverifieerd."
+      echo "$FILE is up-to-date and verified."
       return 0
     fi
   fi
   
-  echo "Downloaden $FILE..."
+  echo "Downloading $FILE..."
   wget -qO "$FILE" "$URL"
   
-  # Hash check na download
   LOCAL_HASH=$(sha256sum "$FILE" | awk '{print $1}')
   if [ "$LOCAL_HASH" != "$EXPECTED_HASH" ]; then
-    echo "FOUT: Hash klopt niet voor $FILE na download. Bestand wordt verwijderd."
+    echo "ERROR: Hash does not match for $FILE after download. File will be deleted."
     rm "$FILE"
     return 1
   else
-    echo "Succes: $FILE geverifieerd."
+    echo "Succes: $FILE verified."
     return 0
   fi
 }
 
-# Voer de checks uit
 check_and_download 'quicksync.raw' 'https://github.com/nerva-project/nerva/releases/download/v0.2.1.0/quicksync.raw'
 check_and_download 'p2pstate.nerva.v11.bin' 'https://github.com/nerva-project/nerva/releases/download/v0.2.1.0/p2pstate.nerva.v11.bin'
 
-# We sluiten ALTIJD af met een succesvolle exit code 0.
-# Mocht een download mislukken, dan faalt deze container niet hard,
-# waardoor 'nervad' (die hier op wacht) alsnog gewoon mag starten.
 exit 0
